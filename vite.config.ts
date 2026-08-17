12047468b66d8409fbdc299f157d44d8f144677b
yotopian <yotopian@gmail.com>
2026-08-17 16:16:15 +0300

    build-v2-entry

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+        v2: fileURLToPath(new URL("./v2.html", import.meta.url)),
+      },
+    },
+  },
+})
